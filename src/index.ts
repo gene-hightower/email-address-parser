24400fcbdf5562e67fc6f678665a2b46d84a2882
Gene Hightower <code@digilicious.com>
2026-05-21 21:46:33 -0700

    inching forward

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export function parse_email_address(address: string) {
         throw new Error("address too long");
     }
 
-    const at_idx = address.lastIndexOf('@'); // One must be found in a valid address.
+    const at_idx = address.lastIndexOf('@')    // One must be found in a valid address.
     if (at_idx === -1) {
         throw new Error("address contains no @");
     }
@@ -35,26 +35,67 @@ export function parse_email_address(address: string) {
     const local_part = address.substring(0, at_idx);
     const domain = address.substring(at_idx + 1);
 
-    if (domain.length > 253) {  // The DNS protocol limit for a domain name, an address literal will be shorter.
+    if (domain.length === 0) {
+        throw new Error("no domain");
+    }
+    if (domain.length > 253) {        // The DNS protocol limit for a domain name, an address literal will be shorter.
         throw new Error("domain too long");
     }
 
-    if (domain[0] === '[') {       // An address literal.
-        // A v4 or v6 address literal.
-    } else {                       // A domain name.
+    if (domain[0] === '[' && domain.at(-1) === ']') {        // A v4 or v6 address literal.
+        // FIXME
+    } else {        // A domain name.
+
+        /* FIXME: A domain in an email address may not contain underscore?
+         */
+
         const labels = domain.split(".");
+
+        labels.forEach((label) => {
+            if (label[0] === '-' || label.at(-1) === '-') {
+                throw new Error("label may not start or end with a ('-') hyphen");
+            }
+            // Valid chars are letter, digit, or hyphen, plus any Unicode.
+            const bad = label.match(/[^A-Za-z0-9\u{80}-\u{10FFFF}-]/u);
+            if (bad.length) {
+                throw new Error(`label may not contain any of "${bad}"`);
+            }
+        });
 
         labels.sort(function(a: string, b: string) {
             return b.length - a.length;
-        });
+        })
         if (labels[0].length > 63) {
             throw new Error("domain label too long");
         }
-        if (labels[-1].length === 0) {            // Two consecutive dots,
-            throw new Error("empty label");       //   or a leading or tailing dot.
+        if (labels.at(-1).length === 0) {      // Two consecutive dots,
+            throw new Error("empty label");    //   or a leading or tailing dot.
         }
     }
 
-    if (local_part[0] === '"' && local_part[-1] === '"') {
+    if (local_part.length === 0) {
+        throw new Error("no local part");
+    }
+
+    if (local_part[0] === '"' && local_part.at(-1) === '"') {
+        // A Quoted-string.
+        const content = local_part.substring(1, local_part.length - 2);
+        const bad = content.match(/\u{0}-\u{1F}\u{22}/u); // FIXME... more chars
+        if (bad.length) {
+            throw new Error(`Quoted-string may not contain any of "${bad}"`);
+        }
+    } else {
+        // A Dot-string.
+        const atoms = domain.split(".");
+        atoms.forEach((atom) => {
+            if (atom.length === 0) {
+                throw new Error("zero length atom in Local-part");
+            }
+            // An atom must not conatin any 'special' chars.
+            const bad = atom.match(/\(\)<>\[\]:;@\\,\./u);
+            if (bad.length) {
+                throw new Error(`atom may not contain any of "${bad}"`);
+            }
+        });
     }
 }
